417da1c2a8c76d0a0c41442e0b6b29c47f2d88ca
Timur Valeyev <116timur116@gmail.com>
2023-02-14 00:43:32 +0600

    refactor styles for input and posts. create onFocus for input.

--- src/redux/components/Header/Header.js
+++ src/redux/components/Header/Header.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import classes from './Header.module.css'
 
 const Header = () => {
+    const [searchValue, setSearchValue] = useState('')
+    const inputRef = useRef()
+
+    const handleSearch = (event) => {
+        setSearchValue(event.target.value)
+    }
+
+    const onSearch = () => {
+        console.log('click')
+        setSearchValue('')
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') onSearch()
+    }
+
+    useEffect(() => {
+        if (inputRef.current) inputRef.current.focus()
+    }, [])
+
     return (
         <header className={classes.header}>
-            <input type="text" placeholder='search'/>
+            <div className={classes.inputBlock}>
+                <input
+                    className={classes.input}
+                    ref={inputRef}
+                    type="text"
+                    placeholder='Please enter something here...'
+                    value={searchValue}
+                    onChange={handleSearch}
+                    onKeyDown={handleKeyDown}
+                />
+                <button className={classes.button} onClick={onSearch}>Search</button>
+            </div>
         </header>
     )
 }
